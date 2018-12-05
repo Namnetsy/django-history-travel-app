6986838ac8e5504ef922621b7e39717ea312289d
Namnetsy <namnesty@gmail.com>
2018-12-05 11:06:22 -0800

    Implement articles list

--- history_travel/history_wiki/urls.py
+++ history_travel/history_wiki/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('page/<int:page_number>/', views.posts_list, name='posts_list'),
     path('categories/', views.categories, name='categories'),
     path('categories/<int:category_id>/', views.category, name='category'),
 ]
--- history_travel/history_wiki/views.py
+++ history_travel/history_wiki/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+
 from .models import Category, Post
+
+POSTS_ON_ONE_PAGE = 2
 
 def index(request):
 	return render(request, 'history_wiki/index.html', {})
+
+def posts_list(request, page_number):
+	context = {}
+	paginator = Paginator(Post.objects.all(), POSTS_ON_ONE_PAGE)
+
+	try:
+		page = paginator.page(page_number)
+		context.update({'page': page.object_list})
+
+		if paginator.page(page_number).has_next():
+			context.update({'next_page': page_number + 1})
+		else:
+			context.update({'next_page': None})
+
+		if paginator.page(page_number).has_previous():
+			context.update({'previous_page': page_number - 1})
+		else:
+			context.update({'previous_page': None})
+	except:
+		context.update({'page': None})
+
+	return render(request, 'history_wiki/posts_list.html', context)
 
 def categories(request):
 	categories = Category.objects.values()
