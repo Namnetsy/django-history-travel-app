d227a731404533573fb54b40bed3cf5c8f38d861
Namnetsy <namnesty@gmail.com>
2018-12-05 08:40:31 -0800

    Replace foreign key from User model to Folk in Post model

--- history_travel/history_wiki/models.py
+++ history_travel/history_wiki/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from froala_editor.fields import FroalaField
 
 class Post(models.Model):
-	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+	author = models.ForeignKey('auth_wiki.Folk', on_delete=models.CASCADE)
 	category = models.ForeignKey('Category', on_delete=models.CASCADE)
 	image = models.ImageField(upload_to='images')
 	title = models.CharField(max_length=200)
